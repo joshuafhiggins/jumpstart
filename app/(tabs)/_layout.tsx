fdf02b66c4fedb2fb373e9dece781f9fb131ece0
Joshua Higgins <joshuafhiggins@gmail.com>
2026-03-10 16:37:14 -0400

    misc: upgrade to expo 55 (#1)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,17 @@
-import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
-import React from "react";
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
+import React from 'react';
 
 export default function TabsLayout() {
-	return (
-		<NativeTabs>
-			<NativeTabs.Trigger name="index">
-				<Icon sf="desktopcomputer" />
-				<Label>Devices</Label>
-			</NativeTabs.Trigger>
-			<NativeTabs.Trigger name="settings">
-				<Icon sf="gear" />
-				<Label>Settings</Label>
-			</NativeTabs.Trigger>
-		</NativeTabs>
-	);
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Icon sf="desktopcomputer" md="home" />
+        <NativeTabs.Trigger.Label>Devices</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <NativeTabs.Trigger.Icon sf="gear" md="settings" />
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
